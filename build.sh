2578bf814450d367dd44f91891f474d460fbc80b
Donkey <donkey@zeugwerk.at>
2022-01-07 20:11:35 +0100

    chore: disabled debug output

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ fi;
 BRANCH=$(echo -n $BRANCH | tr "/" "-")
 
 curl -s --fail --show-error --write-out %{http_code} -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "filepath=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkbuild" https://zeugwerk.at/api.php
-echo "HTTP_CODE: $?"
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
