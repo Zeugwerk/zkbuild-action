b068382536073370a563dc3692bca3c39c8bfdc2
donkey <84121166+iadonkey@users.noreply.github.com>
2022-01-13 16:59:26 +0100

    feat: download build script from CI server

--- build.sh
+++ build.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/env bash
 
-BRANCH=$GITHUB_HEAD_REF
-if [ "$BRANCH" == "" ]; then
-    BRANCH=$(echo $GITHUB_REF | sed 's/refs\/heads\///');
-fi;
-BRANCH=$(echo -n $BRANCH | tr "/" "-")
-
-curl -s -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "tcversion=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkbuild" https://zeugwerk.at/api.php | tee response
-status="$(tail -n1 response)"
-if [[ "$status" != *"HTTP/1.1 200"* ]]; then
-    exit 1
-fi
-
-ARTIFACT_MD5=`printf '%s' "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" | md5sum | awk '{print $1}'`
-ARTIFACT="$1_zkbuild_$ARTIFACT_MD5.zip"
-wget https://zeugwerk.at/public/$ARTIFACT
-if [[ $? -ne 0 ]]; then
-    exit 1
-fi
-
-unzip -o $ARTIFACT
-if [[ $? -ne 0 ]]; then
-    exit 1
-fi
+wget -q https://zeugwerk.dev/Zeugwerk_CI/scripts/build_github -o build_github
+bash build_github
