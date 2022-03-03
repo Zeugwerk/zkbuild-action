6ed529feffbab02c550d54e6bfd48d1b036c1e1c
Donkey <donkey@zeugwerk.at>
2022-03-03 13:56:44 +0100

    fix: changed uri

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "$BRANCH" == "" ]; then
 fi;
 BRANCH=$(echo -n $BRANCH | tr "/" "-")
 
-curl -s -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "tcversion=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkbuild" https://zeugwerk.at/api.php | tee response
+curl -s -N -G --data-urlencode "scm=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --data-urlencode "sha=$GITHUB_SHA" --data-urlencode "branch=$BRANCH" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "tcversion=$3" --data-urlencode "working-directory=$4" --data-urlencode "method=zkbuild" https://wp13699039.server-he.de/api.php | tee response
 status="$(tail -n1 response)"
 if [[ "$status" != *"HTTP/1.1 200"* ]]; then
     exit 1
